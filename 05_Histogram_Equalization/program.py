e11472a80ae0a715e3d9e39ed0cc1c12736464b7
Vishal-Bhardwaj <vbbhardwaj112@gmail.com>
2026-09-13 11:21:29 +0530

    Update Program 05

--- 05_Histogram_Equalization/program.py
+++ 05_Histogram_Equalization/program.py
@@ -1,9 +1,14 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 img = cv2.imread("input2.jpg",0)
 equalized = cv2.equalizeHist(img)
-cv2.imshow("output.png",img)
-cv2.imshow("equalized",equalized)
+cv2.imshow("Original",img)
+cv2.imshow("Equalized",equalized)
 cv2.imwrite("output.png",equalized)
+plt.hist(img.ravel(), bins=256, range=[0,256], alpha=0.5, label="Before")
+plt.hist(equalized.ravel(), bins=256, range=[0,256], alpha=0.5, label="After")
+plt.legend()
+plt.savefig("histogram_comparison.png")
 cv2.waitKey(0)
 cv2.destroyAllWindows()
